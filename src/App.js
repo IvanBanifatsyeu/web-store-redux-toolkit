b578b7ae8f0e0559b85546c7dcc1825b7150c354
Ivan <108135420+IvanBanifatsyeu@users.noreply.github.com>
2024-08-06 17:34:50 +0300

    Building Slider using Redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Main from "./Components/Main/Main";
 import "./App.css";
+import Slider from "./Components/Slider/Slider";
 
 function App() {
 	return (
 		<div className="App">
 			<Main />
+			<Slider />
 		</div>
 	);
 }
