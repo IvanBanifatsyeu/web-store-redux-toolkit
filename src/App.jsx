cea04a2679a8b240d9786f1d1b3948d7c1449a6b
Ivan <108135420+IvanBanifatsyeu@users.noreply.github.com>
2024-08-08 18:43:31 +0300

    Building Navigation Buttons for filtering data

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Main from "./Components/Main/Main";
 import "./App.css";
 import Slider from "./Components/Slider/Slider";
+import NavigateButtons from "./Components/NavigateButtons/NavigateButtons";
+
 
 function App() {
 	return (
 		<div className="App">
 			<Main />
 			<Slider />
+			<NavigateButtons />
 		</div>
 	);
 }
